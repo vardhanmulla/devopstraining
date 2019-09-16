85f80c985a790d87489397d18cc9d6b3a0edafa1
Vardhan Mulla <vardhant@gmail.com>
2019-09-16 07:05:58 +0000

    Initial Commit

---
+++ calc.py
@@ -0,0 +1,12 @@
+def add (x,y):
+   **Add function**
+   return x + y
+def substract (x,y):
+   **substarction function**
+   return x - y
+def multiple (x,y):
+   **multiply function**
+   return x * y
+def add (x,y):
+   *division function**
+   return x / y
